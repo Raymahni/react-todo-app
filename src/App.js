cd0d794347d53d976d96e344791531ea33fdfd30
Raymahni-sub <busram2009@yahoo.com>
2023-05-18 16:31:07 -0600

    Added Wallet chat integration

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React, { useEffect } from "react";
 // Style Imports
 import "./App.css";
 
@@ -6,6 +7,10 @@ import Header from "./components/shared/Header/Header";
 import Sidebar from "./components/shared/Sidebar/Sidebar";
 import CreateTask from "./components/CreateTask/CreateTask";
 import Dashboard from "./components/Dashboard/Dashboard";
+import { WalletChatWidget } from "react-wallet-chat-v0";
+import "react-wallet-chat-v0/dist/index.css";
+
+const { useState } = React;
 
 function App() {
   // This is needed for initial load of the app, checking to see if tasks and collaborators has been set into local storage and sets an empty array for them if not
@@ -16,6 +21,19 @@ function App() {
   if (!JSON.parse(localStorage.getItem("collaborators"))) {
     localStorage.setItem("collaborators", JSON.stringify([]));
   }
+
+  const [widgetState, setWidgetState] = useState({});
+
+  // Usinf Functional update approach
+  useEffect(() => {
+    setWidgetState((w) => {
+      return {
+        ...w,
+        chatAddr: "0x17FA0A61bf1719D12C08c61F211A063a58267A19",
+        isOpen: true,
+      };
+    });
+  }, [setWidgetState]);
 
   return (
     <div className="App">
@@ -29,6 +47,7 @@ function App() {
           <CreateTask />
         </div>
       </div>
+      <WalletChatWidget widgetState={widgetState} />
     </div>
   );
 }
